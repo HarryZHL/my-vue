f1b7472e6db56144d32de7f0019afb466f1dc920
zhanghelong <zhanghelong@wanda.cn>
2017-05-11 17:08:27 +0800

    add tabs

---
+++ src/components/tabs.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="tabs">
+        <ul>
+            <li v-for="(tab,index) in tabs" :class="tab.isCurrent ? 'cur' : ''" @click="changeTab(index)">{{tab.name}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        props:{
+            tabs:{
+                type:Array,
+                default:function () {
+                    return [{
+                        name: '标签1',
+                        isCurrent: false
+                    },{
+                        name: '标签2',
+                        isCurrent: true
+                    },{
+                        name: '标签3',
+                        isCurrent: false
+                    }]
+                }
+            }
+        },
+        methods:{
+            changeTab(index){
+                this.tabs.map((v, i) => {
+                    i == index ? v.isCurrent = true : v.isCurrent = false
+                });
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .tabs{
+        text-align:center;
+        ul{
+            display: table;
+            width: 100%;
+            table-layout: fixed;
+        }
+        li{
+            display: table-cell;
+            margin: 0 10px;
+            line-height:40px;
+            &.cur{
+                background-color:#20a0ff;
+                color: #fff;
+             }
+        }
+    }
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,55 +1,56 @@
 <template>
     <div>
         <div :style="{marginBottom:'10px'}">
-            <img :src="logo" alt="" />
+            <img :src="logo" alt="" class="vue-logo" />
             <el-radio class="radio" v-model="radio" label="1">备选项</el-radio>
             <el-radio class="radio" v-model="radio" label="2">备选项</el-radio>
         </div>
         <el-radio-group v-model="radio3" :size="'large'" :style="{marginBottom:'10px'}">
             <el-radio-button v-for="city in cities" :label="city" :key="city"></el-radio-button>
         </el-radio-group>
+        <Tabs :tabs="tabsArray" :style="{width:'600px',margin:'0 auto'}"></Tabs>
     </div>
 </template>
 
 <script lang="babel">
     import logo from '../assets/logo.png';
+    import Tabs from '../components/tabs.vue';
+
     const cityOptions = ['Shanghai','Beijing','Guangzhou','Shenzhen'];
 
     export default {
+        components:{
+            'Tabs':Tabs
+        },
         data () {
             return {
                 logo: logo,
                 cities:cityOptions,
                 radio: '1',
-                radio3:'Shanghai'
+                radio3:'Shanghai',
+                tabsArray:[{
+                    name:'Shanghai',
+                    isCurrent:true
+                },{
+                    name:'Beijing',
+                    isCurrent:false
+                },{
+                    name:'Guangzhou',
+                    isCurrent:false
+                },{
+                    name:'Shenzhen',
+                    isCurrent:false
+                }]
             }
         }
     }
 </script>
 
 <style lang="scss">
-    img{
+    .vue-logo{
         width: 100px;
         height: 100px;
         display: block;
         margin: 0 auto;
-    }
-
-    h1, h2 {
-        font-weight: normal;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
     }
 </style>
